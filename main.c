04e717f7ef7c327203412de895a2aece1f9d42de
Payden McBee <c15payden.mcbee@usafa.edu>
2013-10-17 22:02:30 -0600

    first commit

---
+++ main.c
@@ -0,0 +1,30 @@
+//Created by: C2C Payden W. McBee
+//Created on: 15 Oct 2013
+//Last Revised: 16 October 2013
+//Purpose: Calculates a moving average
+#include <msp430.h> 
+#include "movingAverage_helper.h"
+
+/*
+ * main.c
+ */
+
+
+void main(void) {
+
+	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	int counter;
+	int numListLength=6;
+	int max;
+	int min;
+	int range;
+    for(counter=0;counter<numListLength;counter++)
+    {
+    average=getAverage(numList[counter]);
+    max=findMaxNum(numList[counter]);
+    min=findMinNum(numList[counter]);
+
+    }
+    range=findRange(max,min);
+	while(1) {}
+}
---
+++ movingAverage_helper.h
@@ -0,0 +1,29 @@
+/*
+ * movingAverage_helper.h
+ *
+ *  Created on: Oct 16, 2013
+ *      Author: C15Payden.McBee
+ *      using code from Capt Branchflower modified
+ * Description: Functions to update and monitor a moving average
+ * and find maximum and minimums of a list of numbers
+ */
+
+#ifndef MOVINGAVERAGE_HELPER_H_
+#define MOVINGAVERAGE_HELPER_H_
+
+int getAverage(int number);
+
+// Array functions
+int findMaxNum(int number);
+int findMinNum(int number);
+
+
+int numList[] = {1,2,3,4,5,6};
+int samplesBeingAveraged[2]={0,0};
+int numSamplesBeingAveraged=2;
+int numSamplesBeingAveraged1Less=1;
+int average;
+
+
+#endif /* MOVINGAVERAGE_HELPER_H_ */
+
